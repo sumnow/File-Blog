e279265249450500c50eccd40a2e71fa100895a0
mervin <mydia@foxmail.com>
2018-01-10 16:42:20 +0800

    update new html

--- js/index.js
+++ js/index.js
@@ -1,9 +1,22 @@
 !function(){
-    $.ajax({
-        url: '138.128.192.220:9000',
-        data: {message: `$m.rint(1,3)`},
-        success: (data) => {
-            console.log(data)
-        }
+    // $.ajax({
+    //     url: '138.128.192.220:9000',
+    //     data: {message: `$m.rint(1,3)`},
+    //     success: (data) => {
+    //         console.log(data)
+    //     }
+    // })
+    // setTimeout(function() {
+        // $('.li-content').addClass('active')
+    // }, 1000);
+    $(window).scroll(()=>{
+        var me = window.scrollY + window.innerHeight;
+        console.log(me)
+        $('.li-content').each(function(i, e){
+            if(e.offsetTop > me) {
+                console.log(this)
+                $(this).addClass('active')
+            }
+        })
     })
 }()
