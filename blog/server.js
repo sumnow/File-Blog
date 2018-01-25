465f56e7181f64c386ac1b11287767aea0966434
mervin <mydia@foxmail.com>
2018-01-25 17:42:19 +0800

    fix little problem last commit   🤦🏻‍♂️

--- blog/server.js
+++ blog/server.js
@@ -8,7 +8,6 @@ var router = require('./fileRoute');
 var port = 8080;
 var pagePath = "/index.html"
 
-
 router.setRootPath(__dirname);
 
 router.get('/', function(req, res){
@@ -19,6 +18,24 @@ router.get('/', function(req, res){
 
 const srv = http.createServer((req, res) => {
     router.init(req, res);
+    const params = url.parse(req.url, true).query;
+    const paths = url.parse(req.url, true).pathname; 
+    // console.log(paths, params.filename);
+    const dir = fs.readdirSync('markdown/down', function (err,files) {
+        return files;
+    })
+
+    const jsondir = JSON.stringify(dir);
+    if (jsondir.includes(params.filename)) {
+        const text = fs.readFileSync(`markdown/down/${params.filename}`, 'utf8');
+        res.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
+        res.write(JSON.stringify(text));
+        res.end();
+    } else if (paths === '/code/catalog') {
+        res.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
+        res.write(jsondir);
+        res.end();
+    } 
 })
 
 // 监听端口
