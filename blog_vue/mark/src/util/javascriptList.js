87a4637a84482ff04a0d5f831dfc7830f6a625a3
mervin <mydia@foxmail.com>
2018-01-26 09:47:44 +0800

    update KeyWordList

--- blog_vue/mark/src/util/javascriptList.js
+++ blog_vue/mark/src/util/javascriptList.js
@@ -2,7 +2,7 @@ export default [{
   keyword: ['function', 'let', 'if', 'else', 'while', 'const', 'var', 'for', 'setTimeout', 'setInterval', 'clearTimeout', 'clearInterval', 'call','apply','bind'],
   color: '#569cd6'
 }, {
-  keyword: ['import', 'return'],
+  keyword: ['import', 'return', 'export', 'default'],
   color: '#c586c0'
 }, {
   keyword: ['Math', 'console', 'this', 'document', 'arguments', 'null', 'undefined', 'Object', 'String', 'Number', 'Array', 'Promise'],
